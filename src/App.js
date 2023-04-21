a12d718c8c1e805fdf2a9c3827089d65b3eb9b6e
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2023-04-21 13:26:07 -0700

    added a style for footer and navbar

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { About, Contact, Dashboard, HomePage } from './pages';
 import { Footer, Navbar } from './components';
+import './App.css';
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<div className='App'>
 				<Navbar />
+				<div className='container'>
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/dashboard' element={<Dashboard />} />
+						<Route path='/Contact' element={<Contact />} />
+						<Route path='/About' element={<About />} />
+					</Routes>
+				</div>
 				<Footer />
 			</div>
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/dashboard' element={<Dashboard />} />
-				<Route path='/Contact' element={<Contact />} />
-				<Route path='/About' element={<About />} />
-			</Routes>
 		</BrowserRouter>
 	);
 };
