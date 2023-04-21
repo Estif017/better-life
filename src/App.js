4d22ec093cb84bc47a02e201ce810ea28c622610
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2023-04-21 11:40:25 -0700

    add a router to the nav bar and added a footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About, Contact, Dashboard, HomePage } from './pages';
+import { Footer, Navbar } from './components';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<Navbar />
+				<Footer />
+			</div>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/dashboard' element={<Dashboard />} />
+				<Route path='/Contact' element={<Contact />} />
+				<Route path='/About' element={<About />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
