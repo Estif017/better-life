fb04b4dc433058ad1af9aafd14374b4fc9413e7b
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2023-04-24 00:37:16 -0700

    added date numbers on the label by ddd,MM,D format

--- src/utils/daysLabel.js
+++ src/utils/daysLabel.js
@@ -1,11 +1,11 @@
 import moment from 'moment';
 const today = moment();
 export const dates = [
-	today.clone().subtract(6, 'days').format('ddd'),
-	today.clone().subtract(5, 'days').format('ddd'),
-	today.clone().subtract(4, 'days').format('ddd'),
-	today.clone().subtract(3, 'days').format('ddd'),
-	today.clone().subtract(2, 'days').format('ddd'),
-	today.clone().subtract(1, 'days').format('ddd'),
-	today.format('ddd'),
+	today.clone().subtract(6, 'days').format('ddd, MMM D'),
+	today.clone().subtract(5, 'days').format('ddd, MMM D'),
+	today.clone().subtract(4, 'days').format('ddd, MMM D'),
+	today.clone().subtract(3, 'days').format('ddd, MMM D'),
+	today.clone().subtract(2, 'days').format('ddd, MMM D'),
+	today.clone().subtract(1, 'days').format('ddd, MMM D'),
+	today.format('ddd, MM D'),
 ];
